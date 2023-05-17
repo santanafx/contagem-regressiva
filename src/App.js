891366eba06523dee5b969d6f342337b0280f4fc
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-17 16:27:55 -0300

    Atualizacao estetica da pagina
    Alteracoes de estetica nos arquivos Counter.css e App.css

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
       <div className='containerRelogio'>
         <Title title='Contagem Regressiva para 2024' />
         <div className='relogio'>
-          <Counter time='dias' number='2' />
-          <Counter time='horas' number='2' />
-          <Counter time='minutos' number='2' />
-          <Counter time='segundos' number='2' />
+          <Counter time='Dias' number='2' />
+          <Counter time='Horas' number='2' />
+          <Counter time='Minutos' number='2' />
+          <Counter time='Segundos' number='2' />
         </div>
       </div>
     </div>
