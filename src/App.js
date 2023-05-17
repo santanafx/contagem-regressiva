14580d7a0d62cb61db19f8287a50ed8baa70c441
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-17 15:54:05 -0300

    Criacao dos componentes Counter e Title
    Adicionado componentes Counter e Title. Adicionado codigo de css em App.css, App.js e index.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Counter } from './components/Counter';
+import { Title } from './components/Title';
+
+import newYear from './imgs/new-year.jpg'
 
 function App() {
   return (
-    <div>
-      teste
+    <div className='App' style={{ backgroundImage: `url(${newYear})` }}>
+      <div className='containerRelogio'>
+        <Title />
+        <div className='relogio'>
+          <Counter title='dias' />
+          <Counter title='horas' />
+          <Counter title='minutos' />
+          <Counter title='segundos' />
+        </div>
+      </div>
     </div>
   );
 }
