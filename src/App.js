dcf9d29fbbdea199670dd6c83a7811258d81fc23
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-05-17 16:54:37 -0300

    Criacao de funcao countDown e uso de useState
    Foi criado a funcao countDown para fazer a logica de contagem de tempo para o ano novo. Foi criado os useState para manipular as variaveis de day, hour, minute e second

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import './App.css';
 import { Counter } from './components/Counter';
 import { Title } from './components/Title';
@@ -5,15 +6,51 @@ import { Title } from './components/Title';
 import newYear from './imgs/new-year.jpg'
 
 function App() {
+
+  const date = 'Jan 1, 2024 00:00:00';
+
+  const [day, setDay] = React.useState();
+  const [hour, setHour] = React.useState();
+  const [minute, setMinute] = React.useState();
+  const [second, setSecond] = React.useState();
+
+  const countDown = (date) => {
+    const countDate = new Date(date).getTime();
+    const countDateNow = new Date().getTime();
+
+    const countDateInterval = countDate - countDateNow;
+
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const dayNumber = Math.floor(countDateInterval / day);
+    const hourNumber = Math.floor((countDateInterval % day) / hour);
+    const minuteNumber = Math.floor((countDateInterval % hour) / minute);
+    const secondNumber = Math.floor((countDateInterval % minute) / second);
+
+
+    setDay(dayNumber);
+    setHour(hourNumber);
+    setMinute(minuteNumber);
+    setSecond(secondNumber);
+
+    console.log(dayNumber, hourNumber, minuteNumber, secondNumber)
+  }
+
+  setInterval(countDown(date), 1000);
+
+
   return (
     <div className='App' style={{ backgroundImage: `url(${newYear})` }}>
       <div className='containerRelogio'>
         <Title title='Contagem Regressiva para 2024' />
         <div className='relogio'>
-          <Counter time='Dias' number='2' />
-          <Counter time='Horas' number='2' />
-          <Counter time='Minutos' number='2' />
-          <Counter time='Segundos' number='2' />
+          <Counter time='Dias' number={day} />
+          <Counter time='Horas' number={hour} />
+          <Counter time='Minutos' number={minute} />
+          <Counter time='Segundos' number={second} />
         </div>
       </div>
     </div>
